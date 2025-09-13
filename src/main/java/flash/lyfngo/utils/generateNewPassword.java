678441117a709191bc01dd8206cca04593e69a55
cheyanv <cheyanv@gmail.com>
2025-09-13 20:20:01 +0530

    Password Generate Randomly Changes Done

--- src/main/java/flash/lyfngo/utils/generateNewPassword.java
+++ src/main/java/flash/lyfngo/utils/generateNewPassword.java
@@ -1,5 +1,7 @@
 package flash.lyfngo.utils;
 
+import java.util.Random;
+
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class generateNewPassword {
@@ -7,13 +9,33 @@ public class generateNewPassword {
 		  String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	        String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 	        String numbers = "0123456789";
-	        String specialCharacters = "!@#$%^&*()-_=+[{]}\\|;:,<.>/";
+	        String specialCharacters = "!@#$%^&*";
+	        Random rnd = new Random();
+	        String newPass;
 
-	        String allCharacters = upperCaseLetters + lowerCaseLetters + numbers + specialCharacters;
+	        do {
+	            StringBuilder sb = new StringBuilder();
+	            // Ensure at least one character from each category
+	            sb.append(upperCaseLetters.charAt(rnd.nextInt(upperCaseLetters.length())));
+	            sb.append(lowerCaseLetters.charAt(rnd.nextInt(lowerCaseLetters.length())));
+	            sb.append(numbers.charAt(rnd.nextInt(numbers.length())));
+	            sb.append(specialCharacters.charAt(rnd.nextInt(specialCharacters.length())));
+	            String all = upperCaseLetters + lowerCaseLetters + numbers + specialCharacters;
+	            for (int i = 0; i < 5; i++) {
+	                sb.append(all.charAt(rnd.nextInt(all.length())));
+	            }
+
+	            newPass = sb.toString();
+	        } while (newPass.equals(oldPassword));
+
+	        return newPass;
+	    }
+	        
+	        /** String allCharacters = upperCaseLetters + lowerCaseLetters + numbers + specialCharacters;
 			  String newpass = RandomStringUtils.random(9, allCharacters);
 			  while (newpass.equals(oldPassword)) {
-				  newpass = RandomStringUtils.random(8, allCharacters);
+				  newpass = RandomStringUtils.random(9, allCharacters);
 			  }
  return newpass;
-	}
+	} **/
 }
